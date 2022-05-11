ac683b9c7918cdd19615fa8ad24891dad6aa7e70
thelaw51 <joegillard3@gmail.com>
2022-05-11 17:02:55 +0100

    started adding remove button functionality to each To-do list item

--- main.js
+++ main.js
@@ -3,21 +3,32 @@ function addToDoItem() {
   const Priority = document.getElementById("PriorityInput").value;
   switch (Priority) {
     case "1":
-      document.getElementById("ListItem1").textContent = newItem;
+      document.getElementById("To-do1").textContent = newItem;
       break;
     case "2":
-      document.getElementById("ListItem2").textContent = newItem;
+      document.getElementById("To-do2").textContent = newItem;
       break;
     case "3":
-      document.getElementById("ListItem3").textContent = newItem;
+      document.getElementById("To-do3").textContent = newItem;
       break;
     case "4":
-      document.getElementById("ListItem4").textContent = newItem;
+      document.getElementById("To-do4").textContent = newItem;
       break;
     case "5":
-      document.getElementById("ListItem5").textContent = newItem;
+      document.getElementById("To-do5").textContent = newItem;
       break;
     default:
       console.log("error incorrect value entered");
   }
+}
+function removeToDoItem1() {
+  document.getElementById("To-do1").textContent =
+    document.getElementById("To-do2").textContent;
+  document.getElementById("To-do2").textContent =
+    document.getElementById("To-do3").textContent;
+  document.getElementById("To-do3").textContent =
+    document.getElementById("To-do4").textContent;
+  document.getElementById("To-do4").textContent =
+    document.getElementById("To-do5").textContent;
+  document.getElementById("To-do5").textContent = "To-Do item";
 }
