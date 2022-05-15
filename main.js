d28e20b2e154ab6a2fcd7cc5890528462a74c364
thelaw51 <joegillard3@gmail.com>
2022-05-15 17:11:16 +0100

    started working on design of the page and finalised remove item functionaility

--- main.js
+++ main.js
@@ -3,59 +3,69 @@ function addToDoItem() {
   const Priority = document.getElementById("PriorityInput").value;
   switch (Priority) {
     case "1":
-      document.getElementById("To-do1").textContent = newItem;
+      document.getElementById("To-do1Text").textContent = newItem;
       break;
     case "2":
-      document.getElementById("To-do2").textContent = newItem;
+      document.getElementById("To-do2Text").textContent = newItem;
       break;
     case "3":
-      document.getElementById("To-do3").textContent = newItem;
+      document.getElementById("To-do3Text").textContent = newItem;
       break;
     case "4":
-      document.getElementById("To-do4").textContent = newItem;
+      document.getElementById("To-do4Text").textContent = newItem;
       break;
     case "5":
-      document.getElementById("To-do5").textContent = newItem;
+      document.getElementById("To-do5Text").textContent = newItem;
       break;
     default:
       console.log("error incorrect value entered");
   }
 }
+
+var input = document.getElementById("ToDoItemInput");
+
+input.addEventListener("keypress", function (event) {
+  if (event.key == "Enter") {
+    event.preventDefault();
+    document.getElementById("addToDoItem").click();
+  }
+});
+
 function removeToDoItem1() {
-  document.getElementById("To-do1").textContent =
-    document.getElementById("To-do2").textContent;
-  document.getElementById("To-do2").textContent =
-    document.getElementById("To-do3").textContent;
-  document.getElementById("To-do3").textContent =
-    document.getElementById("To-do4").textContent;
-  document.getElementById("To-do4").textContent =
-    document.getElementById("To-do5").textContent;
-  document.getElementById("To-do5").textContent = "To-Do item";
+  document.getElementById("To-do1Text").textContent =
+    document.getElementById("To-do2Text").textContent;
+  document.getElementById("To-do2Text").textContent =
+    document.getElementById("To-do3Text").textContent;
+  document.getElementById("To-do3Text").textContent =
+    document.getElementById("To-do4Text").textContent;
+  document.getElementById("To-do4Text").textContent =
+    document.getElementById("To-do5Text").textContent;
+  document.getElementById("To-do5Text").textContent = "To-Do item";
 }
 
 function removeToDoItem2() {
-  document.getElementById("To-do2").textContent =
-    document.getElementById("To-do3").textContent;
-  document.getElementById("To-do3").textContent =
-    document.getElementById("To-do4").textContent;
-  document.getElementById("To-do4").textContent =
-    document.getElementById("To-do5").textContent;
-  document.getElementById("To-do5").textContent = "To-Do item";
+  document.getElementById("To-do2Text").textContent =
+    document.getElementById("To-do3Text").textContent;
+  document.getElementById("To-do3Text").textContent =
+    document.getElementById("To-do4Text").textContent;
+  document.getElementById("To-do4Text").textContent =
+    document.getElementById("To-do5Text").textContent;
+  document.getElementById("To-do5Text").textContent = "To-Do item";
 }
 
 function removeToDoItem3() {
-  document.getElementById("To-do3").textContent =
-    document.getElementById("To-do4").textContent;
-  document.getElementById("To-do4").textContent =
-    document.getElementById("To-do5").textContent;
-  document.getElementById("To-do5").textContent = "To-Do item";
+  document.getElementById("To-do3Text").textContent =
+    document.getElementById("To-do4Text").textContent;
+  document.getElementById("To-do4Text").textContent =
+    document.getElementById("To-do5Text").textContent;
+  document.getElementById("To-do5Text").textContent = "To-Do item";
 }
 
 function removeToDoItem4() {
-  document.getElementById("To-do4").textContent =
-    document.getElementById("To-do5").textContent;
-  document.getElementById("To-do5").textContent = "To-Do item";
+  document.getElementById("To-do4Text").textContent =
+    document.getElementById("To-do5Text").textContent;
+  document.getElementById("To-do5Text").textContent = "To-Do item";
 }
 function removeToDoItem5() {
-  document.getElementById("To-do5").textContent = "To-Do item";
+  document.getElementById("To-do5Text").textContent = "To-Do item";
 }
