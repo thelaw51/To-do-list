c6b4da1889ce3a4fc42bf2b45c23ec8aa2eec6b3
thelaw51 <joegillard3@gmail.com>
2022-07-12 19:44:06 +0100

    attempt  to change the functionality of  the remove item button to just delete the top item from  the list rather then have to type in the the item you want to remove

--- main.js
+++ main.js
@@ -3,12 +3,13 @@ function addToDoItem() {
   var newItem = document.getElementById("ToDoItemInput");
   var li = document.createElement("li");
   li.setAttribute("id", newItem.value);
+  li.setAttribute("class", "listItem");
   li.appendChild(document.createTextNode(newItem.value));
   ul.appendChild(li);
 }
-function removeItem(){
+function removeItem() {
   var ul = document.getElementById("To-doList");
-  var removeItem = document.getElementById("ToDoItemInput");
+  var removeItem = document.getElementsByName("listItem")[0];
   var li = document.getElementById(removeItem.value);
   ul.removeChild(li);
 }
@@ -21,5 +22,3 @@ input.addEventListener("keypress", function (event) {
     document.getElementById("addToDoItem").click();
   }
 });
-
-
